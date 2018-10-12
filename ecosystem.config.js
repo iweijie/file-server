49d574679e7b5d6bd60015e293e101f630818033
weijie <375030086@qq.com>
2018-10-12 10:39:44 +0800

    新增pm2启动文件

---
+++ ecosystem.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+    // First application
+    {
+      name      : 'file-service',
+      script    : './app',
+      watch     :  true ,
+      ignore_watch : ["node_modules"],
+      max_memory_restart : "200M" ,
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'node',
+      host : '47.104.199.117',
+      ref  : 'origin/master',
+      repo : 'git@github.com:weijie9520/file-server.git',
+      path : '/home/blog/file/file-server',
+      'post-deploy' : 'yarn install && pm2 reload ecosystem.config.js --env production'
+    },
+  }
+};
